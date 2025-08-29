7573921081270f04c09ab2181246b706f88c875a
Fahimpro-jpg <fahadalamfahim111844@gmail.com>
2025-08-29 04:02:24 -0700

    2 buttons funciton are done

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// functions
+// coin reduction function
+// function to get innerText
+     function getInnerText(id){
+     const element = document.getElementById(id)
+     const elementValue = element.innerText
+     const elementValueNumber = parseInt(elementValue)
+     return elementValueNumber;
+     }
+
+     // function to set new value
+      function setInnerText(id, value){
+     const element = document.getElementById(id);
+      element.innerText = value;
+}
+
+
+
 
 // heart button function
 let heartCount = 0;
@@ -23,45 +41,157 @@ let heartCount = 0;
 
 // card 1
 document.getElementById("call-button-emergency").addEventListener("click", function(){
-    alert('Calling National Emergency Service')
+    
+    
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
+
+     // Add to history (service name + number)
+    const entry = document.createElement("p");
+    entry.className = "p-2 border-b border-gray-300";
+    entry.innerHTML = `<strong>${serviceName}</strong> - ${number}`;
+    historyContainer.appendChild(entry);
 })
 // card 2
 document.getElementById("call-button-police").addEventListener("click", function(){
-    alert('Calling Police Helpline ')
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 3
 document.getElementById("call-button-fire").addEventListener("click", function(){
-    alert('Calling Fire Service')
+    
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 4
 document.getElementById("call-button-ambulance").addEventListener("click", function(){
-    alert('Calling Ambulance Service')
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 5
 
 document.getElementById("call-button-wc").addEventListener("click", function(){
-    alert('Calling Women & Child Helpline')
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 6
 document.getElementById("call-button-corruption").addEventListener("click", function(){
-    alert('Calling Anti-Corruption Helpline')
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 7
 document.getElementById("call-button-electricity").addEventListener("click", function(){
-    alert('Calling Electricity Helpline')
+     // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 8
 document.getElementById("call-button-ngo").addEventListener("click", function(){
-    alert('Calling Brac Helpline')
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
 
 // card 9
 document.getElementById("call-button-railway").addEventListener("click", function(){
-    alert('Calling Bangladesh Railway Helpline')
+    // coin deduction output
+    const availableCoins = getInnerText("available-coins")
+     if (availableCoins < 20) {
+        alert("❌ You don’t have enough coins to make this call. To make a call need 20 coins");
+        return;
+    }
+
+    alert('📞 Calling National Emergency Service 999');
+
+    const newAvailableCoins = availableCoins - 20;
+    setInnerText("available-coins", newAvailableCoins);
 })
+
+
+
+// clear button
+const historySection = document.createElement("div");
+historySection.id = "call-history-list";
+document.querySelector(".right-side-section").appendChild(historySection);
+
+
+document.getElementById("clear-data").addEventListener("click", function() {
+  historySection.innerHTML = "";
+  console.log('clear data')
+});
